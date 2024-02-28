2fef54c3c994dd3eec5cf30e10bb89c4fecb7817
Daniel Fry <daniel.fry@bjss.com>
2024-02-28 10:23:17 +0000

    add class to create input file for remediation app

--- src/main/java/vott/models/dto/seeddata/AdrRemediationClassGenerator.java
+++ src/main/java/vott/models/dto/seeddata/AdrRemediationClassGenerator.java
@@ -1,7 +1,6 @@
 package vott.models.dto.seeddata;
 
 import vott.models.dto.techrecordsv3.AdrRemediationClass;
-import vott.models.dto.techrecordsv3.TechRecordCarComplete;
 
 public class AdrRemediationClassGenerator extends AbstractTechRecordGenerator<AdrRemediationClass> {
 
